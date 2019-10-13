c0cfaa651ee356560e178180a97310460b113396
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-10-14 06:09:17 +0700

    Created Registry class

---
+++ framework/Registry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Soda;
+
+class Registry
+{
+    protected static $instances = [];
+
+    public static function getAll()
+    {
+        return self::$instances;
+    }
+
+    public static function get($key, $default = null)
+    {
+        return self::$instances[$key] ?? $default;
+    }
+
+    public static function set($key, $value)
+    {
+        self::$instances[$key] = $value;
+    }
+
+    public static function erase($key)
+    {
+        unset(self::$instances[$key]);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,4 +5,7 @@ define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require ROOT_PATH . 'vendor/autoload.php';
 
 $app = new Soda\Application('dev', true);
+
+Soda\Registry::set('app', $app);
+
 $app->start();
